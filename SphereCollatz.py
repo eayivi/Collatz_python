da4df83bab87649d2903d96fbbe01dfaceab75e9
eayivi <enam.ayivi@utexas.edu>
2013-06-10 22:03:00 -0500

    Significant speed improvement

--- SphereCollatz.py
+++ SphereCollatz.py
@@ -43,29 +43,6 @@ def cycle_length (n) :
     return c
 
 
-
-
-
-# def cycle_length (n) :
-
-#     assert ( n > 0);
-
-
-#     c =1
-
-#     while (n>1) :
-#         if ( n%2 ==0) :
-#             n = n/2
-#         else :
-#             n = 3*n +1
-#         c = c+1
-
-#     return c
-
-
-
-
-
 # ------------
 # collatz_read
 # ------------
